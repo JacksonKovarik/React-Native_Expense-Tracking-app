8ab84b232fd39b37f5eb70a8f8aeb12977513b47
JacksonKovarik <jkovarik@umich.edu>
2024-02-03 21:36:32 -0500

    The app files are setup with most of the installations needed installed. The addition of the 'src' folder contains the 'Screens', and 'Utils' folders where the apps screens will be and the apps utilities. Lastly, the 'assets' folder is for images and fonts used within the app.

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+
+
+function App () {
+  return (
+    <View style = {{flex:1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Hello World</Text>
+    </View>
+  )
+}
+
+export default App;
